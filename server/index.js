967d1a3f6056606e6ab9b0c2195d70d54ef1e1c0
devonhackley <devonhackley@gmail.com>
2017-03-13 18:21:34 -0700

    achieved hello world

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+// const Promise = require('bluebird');
+// const mongoose = require('mongoose');
+// const debug = require('debug')('blog-site:backserver');
+
+dotenv.load();
+
+// const PORT = process.env.PORT;
+const app = module.exports = express();
+
+app.use(cors());
+app.use(morgan(process.env.LOG_FORMAT));
+
+
+app.use((err,req,res,next) => {
+  console.log(err);
+  if(err.status){
+    return res.sendStatus(err.status);
+  }
+  next();
+  res.sendStatus(500);
+});

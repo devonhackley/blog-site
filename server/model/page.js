a33c02bda199cf9cea6d39572b761abd8b122247
devonhackley <devonhackley@gmail.com>
2017-03-15 11:51:51 -0700

    working on page model and route

--- server/model/page.js
+++ server/model/page.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const mongoose = require(mongoose);
+
+const pageSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type:String, required:true},
+
+});
+
+module.exports = mongoose.model('page', pageSchema);
